82667ee76112a3ea0e26ac3d4e975c18046ca18d
Rimiru-Tempest <119645132+Rimiru-Tempest@users.noreply.github.com>
2022-12-02 14:44:37 +0530

    C

---
+++ .idea/Variables/Declaring a Variable.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    // declaring  variables
+    int a = 0;
+    int b = 0;
+    int sum = 0;
+
+    printf("Input number 1 : ");
+    scanf("%d",&a);
+
+    printf("Input number 2 : ");
+    scanf("%d",&b);
+
+    sum = a + b;
+
+    printf("Sum = %d",sum);
+
+    return 0;
+}
